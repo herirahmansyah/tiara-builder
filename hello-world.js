201a6daabb0d653128a5a723ced53c106d083bbe
Heri Rahmansyah <rahmansyahheri@gmail.com>
2026-03-14 09:07:28 +0800

    Update: Hello from Tiara-Builder

--- hello-world.js
+++ hello-world.js
@@ -1,13 +1,11 @@
 const http = require('http');
 const hostname = '0.0.0.0';
 const port = 3000;
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.end('Hello from Tiara-Builder\n');
 });
-
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
